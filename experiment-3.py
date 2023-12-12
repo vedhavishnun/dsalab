873ce50b3c52909cd41c353e8d92da1214ebdb09
vedhavishnun <vedhavishnun@gmail.com>
2023-12-12 09:41:49 +0530

    Experiment-3.py

---
+++ experiment-3.py
@@ -0,0 +1,13 @@
+import array
+arr=array.array('i',[1,2,3])
+print(arr)
+arr.append(4)
+print(arr)
+arr.insert(0,0)
+print(arr)
+arr.remove(2)
+print(arr)
+arr.pop()
+print(arr)
+arr.reverse()
+print(arr)
